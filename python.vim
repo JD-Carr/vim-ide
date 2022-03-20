0e56ef8b743a1da4c921b47fe3ea2f19d06a9fac
JD-Char <jordancarr@gmail.com>
2022-03-19 21:43:00 -0500

    Fix setup script

--- python.vim
+++ python.vim
@@ -1,61 +1,29 @@
+" Language: Python
 " PEP8 Compliance {{{
-setlocal fileformat=unix
-" setlocal smartindent
-setlocal autoindent
-setlocal expandtab      " Tab -> spaces
-setlocal shiftround     " Round indent to multiple of `shiftwidth`
-setlocal shiftwidth=4   " width to indent from cindent or indent
-setlocal smarttab       " Set tabs for shiftwidth
-setlocal softtabstop=4  " <Tab> and <Backspace> character length
-setlocal tabstop=4      " 4 character widths for tabs
+setlocal fileformat=unix " LF line endings
+
+setlocal autoindent      " Automatically indent based on current indention
+setlocal smartindent     " Automatically change indentation based on syntax, ignored by `cindent` and `indentexpr`
+setlocal shiftwidth=4    " width to indent from indent or `cindent`
+setlocal shiftround      " Round indent to multiple of `shiftwidth`
+
+setlocal expandtab       " Automatically convert <Tab> to spaces
+setlocal smarttab        " Inserts tabs based on shiftwidth
+setlocal tabstop=4       " 4 character widths for tabs
+setlocal softtabstop=4   " <Tab> and <Backspace> character length
 " }}}
 
 " Folding {{{
-setlocal foldmethod=indent
-" Without next, blank lines would start new fold...
-setlocal foldignore=
-setlocal foldnestmax=2
+setlocal foldmethod=indent " Fold based on indentation, possibly change to syntax?
+setlocal foldignore=       " Ignore blank lines so they do not start a new fold
+setlocal foldnestmax=2     " Don't fold first instance of foldmethod
 " }}}
 
 " Searching {{{
-setlocal suffixesadd+=.py
+setlocal suffixesadd+=.py " prioritize searching for `.py` files
 " }}}
 
-let b:ale_linters = ['pylsp']
-let b:ale_fixers = ['yapf', 'isort']
-
-" Formatoptionen setzen:
-" a: automatic formatting of paragraphs when insering or deleting (nervt!!!)
-" c: automatic formatting for comments
-" t: automatic formatting for text
-" r: repeat comment chars
-" j: remove comment leader when joining lines
-" n: numbered lists
-" q: reformat comments with 'gq'
-" l: Long lines are not broken in insert mode
-" o: automatically insert the current comment leader after 'o' or 'O'
-" w: trailing white space indicates a paragraph continues in the next line
-
-
-" Turn off settings in 'formatoptions' relating to comment formatting.
-" - c : do not automatically insert the comment leader when wrapping based on
-"    'textwidth'
-" - o : do not insert the comment leader when using 'o' or 'O' from command mode
-" - r : do not insert the comment leader when hitting <Enter> in insert mode
-" Python: not needed
-" C: prevents insertion of '*' at the beginning of every line in a comment
-setlocal formatoptions-=c formatoptions-=o formatoptions-=r
-
-" automatically close {[(
-inoremap <buffer> [ []<esc>i
-inoremap <buffer> ( ()<esc>i
-inoremap <buffer> { {}<esc>i
-" automatically close '"
-inoremap <buffer> " ""<esc>i
-inoremap <buffer> ' ''<esc>i
-
-" {{{ Mappings
-" CTRL K moves to the function definition above
-" CTRL J moves to the function definition below
-nmap <leader>k [pf
-nmap <leader>j ]pf
+" Documentation key: `K` {{{
+" Typing `K` while in Normal mode will open documentation for whatever word the cursor is on
+setlocal keywordprg=pydoc " Use python documentation for `.py` files
+" }}}
--- setup_vim.sh
+++ setup_vim.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# vim: set ft=sh:
 # vim version 8.0+ automatically support plugins under a `pack` directory
 # package must follow a guideline in order to automatically start or start upon :<packagename> command
 # <some name> can be any chosen name, `themes` or `syntax` if you want to logically group packages
@@ -20,20 +21,39 @@
 # needs to occur first so `git foreach` updating won't fail
 # Not strictly needed for first installation, but here for posterity
 
-#set -o nounset  # exit with non-zero status if expansion is attempted on an unset variable
-#set -o errexit  # exit immediatelly if a pipeline, a list, or a compound command fails
-#set -o pipefail # failures in pipe in the commands before last one, also count as failures
-#set -o xtrace   # explicitly print out executed commands, for debugging
+vim_config_dir="~/.vim"
+[[ ! -d "${vim_config_dir}" ]] && mkdir "${vim_config_dir}"
 
-afterftdir="${HOME}/.local/share/vim/after/ftplugin"
-[[ ! -d "${afterftdir}" ]] && mkdir --parents "${afterftdir}"
+afterdir="${vim_config_dir}/after"
+[[ ! -d "${afterdir}" ]] && mkdir "${afterdir}"
 
-mv ./.vimrc "${HOME}/.vim"
-mv ./python.vim "${afterftdir}"
+ftdetectdir="${afterdir}/ftdetect"
+[[ ! -d "${ftdetectdir}" ]] && mkdir "${ftdetectdir}"
 
-packdir="${HOME}/.local/share/vim/pack"
+ftplugindir="${afterdir}/ftplugin"
+[[ ! -d "${ftplugindir}" ]] && mkdir "${ftplugindir}"
+mv ./python.vim "${ftplugindir}"
 
-[[ ! -d "${packdir}" ]] && mkdir --parents "${packdir}"
+indentdir="${afterdir}/indent"
+[[ ! -d "${indentdir}" ]] && mkdir "${indentdir}"
+
+syntaxdir="${afterdir}/syntax"
+[[ ! -d "${syntaxdir}" ]] && mkdir "${syntaxdir}"
+
+packdir="${vim_config_dir}/pack"
+[[ ! -d "${packdir}" ]] && mkdir "${packdir}"
+
+backupdir="${vim_config_dir}/backup"
+[[ ! -d "${backupdir}" ]] && mkdir "${backupdir}"
+
+swapdir="${vim_config_dir}/swap"
+[[ ! -d "${swapdir}" ]] && mkdir "${swapdir}"
+
+undodir="${vim_config_dir}/undo"
+[[ ! -d "${undodir}" ]] && mkdir "${undodir}"
+
+viewdir="${vim_config_dir}/view"
+[[ ! -d "${viewdir}" ]] && mkdir "${viewdir}"
 
 cd "${packdir}"
 
@@ -53,14 +73,14 @@ git commit -m "Initial commit"
 pluginsdir=./plugins/start
 mkdir --parents "${pluginsdir}"
 
-git submodule add --depth 1 git://github.com/dense-analysis/ale.git "${pluginsdir}/ale"
+git submodule add --depth 1 https://github.com/dense-analysis/ale.git "${pluginsdir}/ale"
 git add .gitmodules "${pluginsdir}/ale"
 git commit -m "Add submodule ale"
 vim -u NONE -c "helptags plugins/start/ale/doc" -c q
 
 # package :: jedi-vim
 # python code completion
-git submodule add --depth 1 git://github.com/davidhalter/jedi-vim.git "${pluginsdir}/jedi-vim"
+git submodule add --depth 1 https://github.com/davidhalter/jedi-vim.git "${pluginsdir}/jedi-vim"
 git add .gitmodules "${pluginsdir}/jedi-vim"
 git commit -m "Add submodule jedi-vim"
 vim -u NONE -c "helptags plugins/start/jedi-vim/doc" -c q
@@ -68,14 +88,14 @@ vim -u NONE -c "helptags plugins/start/jedi-vim/doc" -c q
 # package :: vim-colorschemes
 # color theme pack with lots of options
 # edit your `.vimrc` file to change the theme
-git submodule add --depth 1 git://github.com/flazz/vim-colorschemes.git "${pluginsdir}/vim-colorschemes"
+git submodule add --depth 1 https://github.com/flazz/vim-colorschemes.git "${pluginsdir}/vim-colorschemes"
 git add .gitmodules "${pluginsdir}/vim-colorschemes"
 git commit -m "Add submodule vim-colorschemes"
 # NOTE: No documentation
 
 # NERDtree browser
 # Haven't fooled with it yet, but looks solid
-# git submodule add git://github.com/scrooloose/nerdtree.git "${pluginsdir}/nerdtree"
+# git submodule add https://github.com/scrooloose/nerdtree.git "${pluginsdir}/nerdtree"
 # git add .gitmodules "${pluginsdir}/nerdtree"
 # git commit -m "Add submodule nerdtree"
 # vim -u NONE -c "helptags plugins/start/nerdtree/doc" -c q
@@ -83,7 +103,7 @@ git commit -m "Add submodule vim-colorschemes"
 git submodule update --init --recursive
 git submodule foreach git submodule update --init --recursive
 # Add file so commit can happen for the main repo
-touch CHANGELOG.rst
+date >> README.rst
 git add .
 git commit -m 'Update submodules to latest versions'
 
